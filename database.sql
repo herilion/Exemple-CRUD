4e903787f66eda8e52182ec00cbe9c9cd463b761
herilion <heritierlionge@yahoo.com>
2022-10-24 11:37:17 +0200

    add values

--- database.sql
+++ database.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS persona(
     id SERIAL PRIMARY KEY,
-    nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(80) NOT NULL,
+    nom VARCHAR(50) NOT NULL,
     sexe VARCHAR(2) DEFAUL 'M',
 );
 INSERT INTO persona(nom,prenom,sexe)
+VALUES
+('Heritier','LIONGE'),
+('Anette','BWEMERE','F'),
+('Alexandre','CHAMBU');
