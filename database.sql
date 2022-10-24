7bd4c3c65c78b19bb72b34f8816c877fc9d5a0e9
herilion <heritierlionge@yahoo.com>
2022-10-24 11:44:50 +0200

    update table

--- database.sql
+++ database.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS persona(
     id SERIAL PRIMARY KEY,
     prenom VARCHAR(80) NOT NULL,
     nom VARCHAR(50) NOT NULL,
-    sexe VARCHAR(2) DEFAUL 'M',
+    sexe VARCHAR(2) DEFAULT 'M'
 );
 INSERT INTO persona(nom,prenom,sexe)
 VALUES
-('Heritier','LIONGE'),
+('Heritier','LIONGE','M'),
 ('Anette','BWEMERE','F'),
-('Alexandre','CHAMBU');
+('Alexandre','CHAMBU','M');
